a2f723b0b9ed6f1c0727e0a110ae8ca337f9d0ac
dngopal <136632602+dngopal@users.noreply.github.com>
2024-09-11 08:12:52 -0400

    Update script.js
    Signed-off-by: dngopal <136632602+dngopal@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+// Smooth scrolling to sections
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault(); // Prevent default anchor click behavior
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth' // Smooth scroll behavior
+        });
+    });
+});
 
